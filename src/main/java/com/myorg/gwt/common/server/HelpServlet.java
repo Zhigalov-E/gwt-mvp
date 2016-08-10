05b95b4e83de36afec432accb20433444dcd79bf
Evgeniy <zhigalov_java@mail.ru>
2016-08-10 04:21:18 +0300

    refactoring

--- src/main/java/com/myorg/gwt/common/server/HelpServlet.java
+++ src/main/java/com/myorg/gwt/common/server/HelpServlet.java
@@ -1,13 +1,11 @@
 package com.myorg.gwt.common.server;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HelpServlet")
 public class HelpServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request,response);
