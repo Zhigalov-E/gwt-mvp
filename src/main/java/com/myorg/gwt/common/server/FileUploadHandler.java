599c354567716931efe7750fe375de22e99c7744
Evgeniy <zhigalov_java@mail.ru>
2016-08-17 02:41:46 +0300

    Check max file size

--- src/main/java/com/myorg/gwt/common/server/FileUploadHandler.java
+++ src/main/java/com/myorg/gwt/common/server/FileUploadHandler.java
@@ -1,9 +1,10 @@
 package com.myorg.gwt.common.server;
 
-import org.apache.commons.fileupload.FileItemIterator;
-import org.apache.commons.fileupload.FileItemStream;
+import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +14,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.List;
 import java.util.regex.Pattern;
 
 
@@ -22,31 +24,31 @@ public class FileUploadHandler extends HttpServlet {
     private static final String CHAR_TERMINATOR = ",";
     private static final String LINE_DELIMITER = "\n";
 
-    private static final String INVALID_LINE_FORMAT = "Invalid line format";
+    private long FILE_SIZE_LIMIT = 1 * 1024 * 1024; // 1 MiB
+    private final Logger logger = Logger.getLogger("UploadServlet");
 
     private static final Pattern NAME_SURNAME_PATTERN = Pattern.compile("^[а-яА-ЯёЁa-zA-Z]{1,40} [а-яА-ЯёЁa-zA-Z]{1,40}$");
     private static final Pattern DATE_PATTERN = Pattern.compile("^[0-3]?[0-9].[0-3]?[0-9].(?:[0-9]{2})?[0-9]{2}$");
     private static final Pattern EMAIL_PATTERN = Pattern.compile("^[-\\w.]+@([A-z0-9][-A-z0-9]+\\.)+[A-z]{2,4}$");
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ServletFileUpload upload = new ServletFileUpload();
+        DiskFileItemFactory fileItemFactory = new DiskFileItemFactory();
+        ServletFileUpload upload = new ServletFileUpload(fileItemFactory);
+        upload.setSizeMax(FILE_SIZE_LIMIT);
+
         BufferedReader bufReader = null;
         PrintWriter responseWriter = null;
         try {
-            FileItemIterator iter = upload.getItemIterator(request);
-            while (iter.hasNext()) {
-                FileItemStream item = iter.next();
-
+            List<FileItem> items = upload.parseRequest(request);
+            for (FileItem item : items) {
                 if(!item.isFormField() && item.getName().toUpperCase().endsWith(FILE_EXTENTION)) {
 
-                    bufReader = new BufferedReader(new InputStreamReader(item.openStream()));
+                    bufReader = new BufferedReader(new InputStreamReader(item.getInputStream()));
                     StringBuilder stringBuilder = new StringBuilder();
                     String line;
                     while ((line = bufReader.readLine()) != null) {
                         if(IsCorrectLine(line)) {
                             stringBuilder.append(line).append(LINE_DELIMITER);
-                        } else {
-                            stringBuilder.append(INVALID_LINE_FORMAT).append(LINE_DELIMITER);
                         }
                     }
                     response.setContentType("text/html; charset=UTF-8");
@@ -56,6 +58,7 @@ public class FileUploadHandler extends HttpServlet {
                 }
             }
         } catch (FileUploadException e) {
+            logger.error("Throwing servlet exception for unhandled exception", e);
             throw new ServletException("Cannot parse multipart request.", e);
         } finally {
             if(bufReader != null) {
