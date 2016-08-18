b58a45861c42b54cee9b94daee25fde6d2cc835a
Evgeniy <zhigalov_java@mail.ru>
2016-08-18 03:29:58 +0300

    json responce

---
+++ src/main/java/com/myorg/gwt/common/server/model/Client.java
@@ -0,0 +1,32 @@
+package com.myorg.gwt.common.server.model;
+
+
+public class Client {
+    private String name;
+    private String date;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
