2e4c881ab92faf5a0ebede6a2544dfa5e158fcf8
Evgeniy <zhigalov_java@mail.ru>
2016-08-19 04:34:44 +0300

    Add record paging client data

--- src/main/java/com/myorg/gwt/file/client/widget/ClientsWidget.java
+++ src/main/java/com/myorg/gwt/file/client/widget/ClientsWidget.java
@@ -6,9 +6,11 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiTemplate;
 import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.SimplePager;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.view.client.ListDataProvider;
 
 import java.util.List;
 
@@ -23,8 +25,12 @@ public class ClientsWidget<T extends IClient> extends Composite {
     @UiField
     CellTable<T> clientTable;
 
+    @UiField
+    SimplePager pager;
+
     public ClientsWidget() {
         initWidget(uiBinder.createAndBindUi(this));
+        pager.setVisible(false);
     }
 
     public void setData(List<T> clients) {
@@ -51,6 +57,14 @@ public class ClientsWidget<T extends IClient> extends Composite {
 
         clientTable.setRowCount(clients.size(), true);
         clientTable.setRowData(0, clients);
+
+        ListDataProvider<T> dataProvider = new ListDataProvider<>();
+        dataProvider.addDataDisplay(clientTable);
+        dataProvider.setList(clients);
+
+        pager.setDisplay(clientTable);
+        pager.setPageSize(20); // 20 rows will be shown at a time
+        pager.setVisible(true);
     }
 
     public void clear() {
@@ -58,6 +72,7 @@ public class ClientsWidget<T extends IClient> extends Composite {
         for (int i = 0; i < columnCount; ++i) {
             clientTable.removeColumn(0);
         }
+        pager.setVisible(false);
     }
 
 
