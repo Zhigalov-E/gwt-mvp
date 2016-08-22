7d1fe3aee49d4942fc2cbc703f91e1fd5de5f739
Evgeniy <zhigalov_java@mail.ru>
2016-08-23 02:21:25 +0300

    Adding ScrollPanel to CellTable

--- src/main/java/com/myorg/gwt/file/client/widget/ClientsWidget.java
+++ src/main/java/com/myorg/gwt/file/client/widget/ClientsWidget.java
@@ -7,11 +7,9 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiTemplate;
 import com.google.gwt.user.cellview.client.CellTable;
-import com.google.gwt.user.cellview.client.SimplePager;
 import com.google.gwt.user.cellview.client.TextColumn;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.gwt.view.client.ListDataProvider;
 import com.myorg.gwt.file.client.mvp.view.css.FileResources;
 
 import java.util.List;
@@ -29,12 +27,8 @@ public class ClientsWidget<T extends IClient> extends Composite {
     @UiField
     CellTable<T> clientTable;
 
-    @UiField
-    SimplePager pager;
-
     public ClientsWidget() {
         initWidget(uiBinder.createAndBindUi(this));
-        pager.setVisible(false);
     }
 
     public void setData(List<T> clients) {
@@ -78,17 +72,10 @@ public class ClientsWidget<T extends IClient> extends Composite {
             }
 
         }, "Email");
-
+        clientTable.setPageSize(clients.size());
         clientTable.setRowCount(clients.size(), true);
         clientTable.setRowData(0, clients);
 
-        ListDataProvider<T> dataProvider = new ListDataProvider<>();
-        dataProvider.addDataDisplay(clientTable);
-        dataProvider.setList(clients);
-
-        pager.setDisplay(clientTable);
-        pager.setPageSize(20); // 20 rows will be shown at a time
-        pager.setVisible(true);
     }
 
     public void clear() {
@@ -96,7 +83,6 @@ public class ClientsWidget<T extends IClient> extends Composite {
         for (int i = 0; i < columnCount; ++i) {
             clientTable.removeColumn(0);
         }
-        pager.setVisible(false);
     }
 
     public void setFileResources(FileResources fileResources) {
